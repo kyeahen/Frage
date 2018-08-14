f33fcdbc89422f10467c4f2860c8f55d24479a77
Daigeun Sohn <daigeun.sohn@navercorp.com>
2018-08-15 02:44:21 +0900

    Add PostFrageViewController

--- Frage/Frage/View/Frage/BoardTableViewCell.swift
+++ Frage/Frage/View/Frage/BoardTableViewCell.swift
@@ -14,18 +14,6 @@ class BoardTableViewCell: UITableViewCell {
     @IBOutlet weak var authorTypeLabel: UILabel!
     @IBOutlet weak var contentsLabel: UILabel!
     
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     public func updateViews(with frage: Frage) {
         authorNameLabel.text = frage.author.name
         authorTypeLabel.text = frage.author.authorType
---
+++ Frage/Frage/View/Frage/PostFrage/PostFrageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PostFrageViewController.swift
+//  Frage
+//
+//  Created by Daigeun Sohn on 15/08/2018.
+//  Copyright © 2018 yeen. All rights reserved.
+//
+
+import UIKit
+
+class PostFrageViewController: UIViewController {
+
+}
+
+// MARK: - View Life Cycle
+extension PostFrageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
